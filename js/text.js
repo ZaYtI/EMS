a22c86215b249ee4f6dfab23c0c7af6d202670df
Riquart Antonin <Antonin_riquart@ens.univ-artois.fr>
2022-06-30 00:35:27 +0200

    Initial commit

---
+++ js/text.js
@@ -0,0 +1,20 @@
+function OnClickDivEntreprise() {
+    window.location.href="gestion_flotte.html";
+
+}
+
+function OnClickDivLoc(){
+    window.location.href="location.html"
+}
+
+function OnClickFacebook(){
+    window.location.href="https://m.facebook.com/EMS-Location-101000738888860/posts"
+}
+
+function OnClickTwitter(){
+    window.location.href=""
+}
+
+function OnClickInstagram(){
+    window.location.href=""
+}
