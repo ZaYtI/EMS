d5ed2c1cbd0f5a9a0865fd4194e08e5fe2658630
antonin <99360974+ZaYtI@users.noreply.github.com>
2022-12-28 17:32:55 +0100

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,17 +1,49 @@
 <?php
-    if (isset($_POST['message'])) {
-        $message="Ce Message vous a était envoyer depuis le site EMS location
-        NOM:" .$_POST["Name"] ."
-        Telephone:" .$_POST["Tel"] ."
-        Date de départ:" .$_POST["Date"] ."
-        Date d'arrive:" .$_POST["DateA"] ."
-        email:" .$_POST["email"] ."
-        Message:" .$_POST["descript"];
-        $retour = mail('antonin.riquart@gmail.com', $_POST["sujet"] , $message, 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST["email"]);
-        if($retour)
-            echo '<p>Votre message a bien été envoyé.</p>';
-        else
-            echo '<p>Votre message ne peux pas etre envoyer verifier les informations transmise';
+
+$entete  = 'MIME-Version: 1.0' . "\r\n";
+$entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$entete .= 'From: antonin.riquart@gmail.com' . "\r\n";
+$entete .= 'Reply-to: ' . $_POST['email'];
+$message_entreprise = '<html">
+            <body style="width: 75%; margin: auto;">
+            <a style="width: 100%; height: 80px;" href="https://zupimages.net/viewer.php?id=22/48/qyrk.png"><img src="https://zupimages.net/up/22/48/qyrk.png" alt="" /></a>
+       
+            <h2 style="text-align: center;">Demande de location</h2>
+            <hr style="height: 5px; background-color: #d7d7d7; width: 80%; margin: auto auto; border: none; border-radius: 5px 5px 5px 5px">
+        <p><b>Sujet : </b>' . htmlspecialchars($_POST['sujet']) . '</p>
+        <p><b>Email : </b>' . htmlspecialchars($_POST['email']) . '</p>
+        <p><b>Nom : </b>' . htmlspecialchars($_POST['Name']) .'</p>
+        <p><b>numero de telephone </b>' . htmlspecialchars($_POST['Tel']) .'</p>
+        <p><b>Date de depart : </b>' . htmlspecialchars($_POST['Date']) .'</p>
+        <p><b>Date de retour : </b>' . htmlspecialchars($_POST['DateA']) . '</p>
+        <p><b>Message : </b>' . htmlspecialchars($_POST['area']) . '</p>
+        <hr style="height: 5px; background-color: #d7d7d7; width: 80%; margin: 10px auto 10px auto; border: none; border-radius: 5px 5px 5px 5px;">
+        <h3 style="text-align: center;">Merci de votre confiance</h3>
+        </body>
+        </html>';
+
+$message = '<html">
+            <body style="width: 75%; margin: auto;">
+            <a style="width: 100%; height: 80px;" href="https://zupimages.net/viewer.php?id=22/48/qyrk.png"><img src="https://zupimages.net/up/22/48/qyrk.png" alt="" /></a>
+       
+            <h3 style="text-align: center;">Retour demande de location</h3>
+            <hr style="height: 5px; background-color: #d7d7d7; width: 80%; margin: auto auto; border: none; border-radius: 5px 5px 5px 5px">
+        <p><b>Sujet : </b>' . htmlspecialchars($_POST['sujet']) . '</p>
+        <p><b>Nom : </b>' . htmlspecialchars($_POST['Name']) .'</p>
+        <p><b>numero de telephone </b>' . htmlspecialchars($_POST['Tel']) .'</p>
+        <p><b>Date de depart : </b>' . htmlspecialchars($_POST['Date']) .'</p>
+        <p><b>Date de retour : </b>' . htmlspecialchars($_POST['DateA']) . '</p>
+        <p><b>Message : </b>' . htmlspecialchars($_POST['area']) . '</p>
+        <hr style="height: 5px; background-color: #d7d7d7; width: 80%; margin: 10px auto 10px auto; border: none; border-radius: 5px 5px 5px 5px;">
+        <h4 style="text-align: center;">Merci de votre confiance</h4>
+        </body>
+        </html>';
+$dest = "antonin.riquart@gmail.com";
+    if(mail($dest,$_POST['sujet'],$message_entreprise,$entete) && mail($_POST['email'],$_POST['sujet'],$message,$entete)){
+        echo 'envoie valide';
+    }
+    else{
+        echo 'erreur d envoie';
     }
 ?>
 
